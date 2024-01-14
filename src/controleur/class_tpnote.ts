4eb58cc9f2fcd5a3fa45ba6acc0044073572ab96
hlothaire <lothaire.holcvart78@gmail.com>
2024-01-14 17:00:13 +0100

    initial commit

---
+++ src/controleur/class_tpnote.ts
@@ -0,0 +1,7 @@
+type TTpnoteForm = {
+  div_affichage: HTMLElement;
+  div_formulaire: HTMLElement;
+  select_salles: HTMLSelectElement;
+  btn_ajouter: HTMLInputElement;
+  btn_retirer: HTMLInputElement;
+};
